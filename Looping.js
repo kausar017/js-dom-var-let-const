1b3494467c7d9ecc86910317fe25f1d0a92a9f77
kousra <mdkousarmia71@gmail.com>
2024-09-26 21:59:04 +0600

    js practios

---
+++ Looping.js
@@ -0,0 +1,24 @@
+const numbars = [1, 2, 3, 4, 5, 6, 7];
+for(const num of numbars) {
+    console.log(num);
+    
+}
+
+const parson = 'md kausar mia im frome Bangladesh';
+for(const man of parson){
+    console.log(man);
+    
+}
+
+const glass =  { 
+    name: 'glass', 
+    color: 'golden', 
+    price: 50, 
+    isClined: true
+};
+
+for( const key in glass){
+    const valu = glass[key];
+    console.log(va);
+    
+}
